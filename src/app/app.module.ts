d9966ac30437e10ce23a7e799c1b22b8cbcb1b55
Sergii_Shust <sergii_shust@gmail.com>
2017-03-05 21:46:20 +0200

    add routing, 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CourseDetailComponent } from './shared/course-detail/course-detail.component';
+import { CourseDetailsComponent } from './shared/course-details/course-details.component';
+import { LoginComponent } from './shared/login/login.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { SearchComponent } from './shared/search/search.component';
+import { HomeComponent } from './home/home.component';
+import {ROUTES} from "./app.routes";
+import {NoContentComponent} from "./no-content/no-content.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CourseDetailComponent,
+    CourseDetailsComponent,
+    LoginComponent,
+    MenuComponent,
+    SearchComponent,
+    HomeComponent,
+    NoContentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES, {useHash: true, preloadingStrategy: PreloadAllModules}),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
