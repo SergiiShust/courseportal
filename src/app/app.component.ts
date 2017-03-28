ffa4bc3396f7cd631c8029d095063df721deca28
Sergii_Shust <sergii_shust@gmail.com>
2017-03-29 00:45:21 +0300

    hw4 - add perfomance tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private _startTime: number;
+
+  constructor(private _ngZone: NgZone) {
+
+  }
+
+  ngOnInit(): void {
+    this._ngZone.onUnstable.subscribe(()=>{
+      this._startTime = Date.now();
+      console.log(`Started onUnstable`)
+    });
+
+    this._ngZone.onStable.subscribe(()=>{
+     console.log(`Finished. onStable. Took ${Date.now() - this._startTime} miliseconds`);
+    });
+  }
 }
