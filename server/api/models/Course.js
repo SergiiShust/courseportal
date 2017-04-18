051d59abda2d16d72432429bd17aa12da188fbb6
Sergii_Shust <sergii_shust@gmail.com>
2017-04-18 23:55:58 +0300

    hw7 - add server

---
+++ server/api/models/Course.js
@@ -0,0 +1,16 @@
+/**
+ * Course.js
+ *
+ * @description :: This model represent learning course in our portal.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    title: {type: 'text',  required: true},
+    date: {type: 'date',  required: true},
+    duration: {type: 'integer',  required: true},
+    topRated: {type: 'boolean',  required: true}
+  }
+};
+
