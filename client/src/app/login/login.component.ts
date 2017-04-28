63f5aaac3e7ec0aa2ade7f918715462e420f957a
Sergii_Shust <sergii_shust@gmail.com>
2017-04-28 12:10:02 +0300

    hw8 - add formGroup to login

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AuthorizationService} from "../common/services/authorization.service";
 import {Router} from "@angular/router";
 import {OverlayService} from "../common/components/overlay/overlay-service/overlay-service.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'trainme-login',
@@ -12,18 +13,23 @@ export class LoginComponent implements OnInit {
 
   constructor(private authorizationService: AuthorizationService,
               private overlayService: OverlayService,
+              private formBuilder: FormBuilder,
               private router: Router) {
   }
 
-  email: string;
-  password: string;
+
+  formModel: FormGroup;
 
   ngOnInit() {
+    this.formModel = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
   login() {
     this.authorizationService
-      .login({email: this.email, password: this.password})
+      .login(this.formModel.value)
       .do(() => {
         this.overlayService.show();
       })
