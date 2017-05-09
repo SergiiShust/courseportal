e2da12e901f8a55085b7da15c47543a5b3ddb2d4
SergiiS <shustsergey@gmail.com>
2017-05-09 16:10:06 +0300

    - 1. Implement routes for app

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,14 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NoContentComponent } from './no-content/no-content.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NoContentComponent} from './no-content/no-content.component';
 import {LoginComponent} from "./login/login.component";
 import {AddCourseComponent} from "./add-course/add-course.component";
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'courses', pathMatch: 'full'},
+  {path: 'courses', component: HomeComponent},
+  {path: 'courses/new', component: AddCourseComponent},
+  {path: 'courses/:id', component: AddCourseComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'addcourse', component: AddCourseComponent},
   {path: '**', component: NoContentComponent}
 ];
